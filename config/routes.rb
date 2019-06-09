f0a7acd99e7dc44a88926b1820d4bdaf63230b9a
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-06-09 20:25:57 -0300

    Adds Auth#login and a routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, params: :_username
+  post '/auth/login', to: 'authentication#login'
+  get '/*a', to: 'application#not_found'
 end
