d19cec0eb988b6be5f8ef74ada09252f6cee077a
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-06-09 17:06:19 -0300

    Adds configuration for generates UUID Primary Key

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,10 @@ module Gastos
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Generates UUID
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
