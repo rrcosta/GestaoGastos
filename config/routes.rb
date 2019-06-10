b0d15ee1affb5925c56e7b9f8d37f75c54011293
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-06-10 10:11:05 -0300

    Adds outlays routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :outlays
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, params: :_username
   post '/auth/login', to: 'authentication#login'
