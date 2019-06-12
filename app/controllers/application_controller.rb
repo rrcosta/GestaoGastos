f978278d1624696edbf18e59b7a42567f0f7d55e
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-06-11 21:45:46 -0300

    Improves return error message in authorize_request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,16 @@ class ApplicationController < ActionController::API
     begin
       @decoded = JsonWebToken.decode(header)
       @current_user = User.find(@decoded[:user_id])
+
     rescue ActiveRecord::RecordNotFound => e
-      render json: { errors: e.message }, status: :unauthorized
+      render json: { 
+                     errors: "Registro não encontrado. Detalhe:: #{e.message}"
+                   }, status: :unauthorized
+
     rescue JWT::DecodeError => e
-      render json: { errors: e.message }, status: :unauthorized
+      render json: { 
+                      errors: "Token nao informado OU invalido. Detalhe:: #{e.message}"
+                   }, status: :unauthorized
     end
   end
 end
