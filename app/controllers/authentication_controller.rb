c4760841d8ce4257410bd601327d9dd9ebbcb5da
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-06-14 11:15:11 -0300

    Improves AuthenticationController#login

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,14 +1,19 @@
+# coding: utf-8
 class AuthenticationController < ApplicationController
   before_action :authorize_request, except: :login
 
   # POST /auth/login
   def login
-    @user = User.find_by_email(params[:email])
+    @user = User.find_by_username(params[:username])
 
     if @user&.authenticate(params[:password])
       token = JsonWebToken.encode(user_id: @user.id)
 
-      render json: { token: token, username: @user.username }, status: :ok
+      render json: {
+                     token: token,
+                     username: @user.username,
+                     name: @user.name
+                   },status: :ok
     else
       render json: { error: 'Usuário não autorizado.' }, status: :unauthorized
     end
