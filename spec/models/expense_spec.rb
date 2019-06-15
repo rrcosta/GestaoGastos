77c3acf42ab52a628bbd816beaabbaa368807c73
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-06-14 22:36:17 -0300

    Removes scopes test

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -10,19 +10,4 @@ RSpec.describe Expense, type: :model do
     it { is_expected.to validate_presence_of(:valor) }
     it { is_expected.to validate_presence_of(:data) }
   end
-
-  context "scopes" do
-    describe '.ordered' do
-      let(:exp1) { create(:expense, descricao: 'Descr1') }
-      let(:exp2) { create(:expense, descricao: 'Descr2') }
-
-      xit 'returns expenses ordered by created_at desc' do
-        expect{
-          Expense.last(2).reverse.map(&:id)
-        }.to eq[exp1.id, exp2.id]
-      end
-
-    end
-  end
-
 end
