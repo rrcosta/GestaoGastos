da79557ee9909b3f018e07ff4ae0c610b994543e
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-06-16 11:12:48 -0300

    Adds associations

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,6 @@
 class Expense < ApplicationRecord
   belongs_to :user
+  has_many :category
 
   validates :descricao, presence: true
   validates :valor, presence: true
@@ -7,4 +8,6 @@ class Expense < ApplicationRecord
 
   scope :ordered, -> { order(data: :desc) }
   scope :filter_by_date, -> (period) { where('DATE(data) = ?', period) }
+
+  accepts_nested_attributes_for :category, allow_destroy: true
 end
