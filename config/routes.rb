8e1a02692463e9c75b1f2f7490bf4239da5dfaa2
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-06-16 14:45:30 -0300

    Adds Category routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :expenses 
+  
+  resources :expenses do
+    get  'categoria',   to: 'categories#index'
+    post 'categoria',   to: 'categories#create'
+  end
+
+  resources :categories, only: [:index]
 
   resources :users, params: :_username
 
   # filtro de data
-  get "gasto_por_dia", to: "expenses#filter_by_date", param: :data
+  get 'gasto_por_dia', to: 'expenses#filter_by_date', param: :data
 
   post '/auth/login', to: 'authentication#login'
   get '/*a', to: 'application#not_found'  
