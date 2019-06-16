8a4472d5ce849d58cfd79fbcadc75966979731e4
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-06-16 15:03:35 -0300

    Adds category associations in expense

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Expense, type: :model do
   context "Associations" do
     it { is_expected.to belong_to :user }
+    it { is_expected.to have_many :category }    
   end
 
   context "Validations" do
