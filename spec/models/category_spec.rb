dd9792d5de2900b1e04bdd0ec93111e02aba7698
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-06-16 19:15:50 -0300

    Improves syntax of category_spec model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+  context "Associations" do
+    it { is_expected.to belong_to :expense }
+  end
+
   context "Validations" do
-
-    context "Associations" do
-      it { is_expected.to belong_to :expense }
-    end
-
     it { is_expected.to validate_presence_of(:descricao) }
   end
 end
